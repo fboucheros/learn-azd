28dac071ed20ffb1d4aa0aff5e13dbb22a68c0d8
Frank Boucher <fboucher@outlook.com>
2024-03-22 09:48:48 -0400

    now its ours

--- Functions/c5mTest.cs
+++ Functions/c5mTest.cs
@@ -22,7 +22,7 @@ namespace learn_azd
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString("Bonjour Hi from Behind my cloud!");
 
             return response;
         }
